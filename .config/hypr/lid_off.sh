c2c60972065df1954b293a19757cf6895a0edce9
G3ordan1 <geordan.ramsamy@gmail.com>
2023-10-23 10:38:02 +0400

    second commit

---
+++ .config/hypr/lid_off.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Run the hyprctl monitors command and store the result
+output=$(hyprctl monitors)
+
+# Check if the output contains ID 1
+if echo "$output" | grep -q "ID 1"; then
+  # Execute the first command
+  echo "ID 1 found. Executing Command 1..."
+  # Replace the following line with your desired command
+  hyprctl keyword monitor "eDP-1, disable"
+else
+  # Execute the second command
+  echo "ID 1 not found. Executing Command 2..."
+  # Replace the following line with your desired command
+  hyprctl dispatch dpms off eDP-1
+fi
